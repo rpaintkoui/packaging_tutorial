eafc4cded61e427a3113d403953ede0608673881
rpaintkoui <ravahere.paint-koui@etu.umontpellier.fr>
2021-03-16 19:22:25 +0100

    Load_db.py modified

--- biketrauma/io/Load_db.py
+++ biketrauma/io/Load_db.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from download import download
-from biketrauma.io import url_db, path_target
+#from biketrauma.io import url_db, path_target
+import os
 
 url_db = "https://www.data.gouv.fr/fr/datasets/r/ab84353b-498b-4ef5-9a02-a6403f2ead96"
 path_target = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "bicycle_db.csv")
