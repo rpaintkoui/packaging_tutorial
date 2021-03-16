2df1aceef7c2b9de6b62d95e5ebeefde914fe744
rpaintkoui <ravahere.paint-koui@etu.umontpellier.fr>
2021-03-16 19:59:52 +0100

    setup file modified to upload the package on PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from biketrauma import __version__ as current_version
 
 setup(
-  name='biketrauma',
+  name='biketrauma_airPekka',
   version=current_version,
   description='Visualization of a bicycle accident db',
   url='http://github.com/rpaintkoui/packaging_tutorial.git',
